d0163bbe8060c55b774a55ef6a63ac7c0b201b15
buffer0x7cd <manvendra021997@gmail.com>
2017-10-04 12:42:00 +0530

    include templates

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 class HomePageTest(TestCase):
     ''' Test if home page is accessible'''
     def test_root_url_resolve_to_homepage_view(self):
@@ -9,9 +10,10 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        responce = home_page(request)
+        responce = self.client.get('/')
+
         html = responce.content.decode('utf-8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
+        self.assertTemplateUsed(responce, 'lists/home.html')
