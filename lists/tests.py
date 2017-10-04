ee05599c624b315ee466e2ea3614e74228b41e46
buffer0x7cd <manvendra021997@gmail.com>
2017-10-05 01:36:27 +0530

    Refactored home page view to use atemplate

--- lists/tests.py
+++ lists/tests.py
@@ -4,16 +4,8 @@ from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 class HomePageTest(TestCase):
-    ''' Test if home page is accessible'''
-    def test_root_url_resolve_to_homepage_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
+        ''' Check if correct template is being used for render'''
         responce = self.client.get('/')
-
-        html = responce.content.decode('utf-8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
         self.assertTemplateUsed(responce, 'lists/home.html')
