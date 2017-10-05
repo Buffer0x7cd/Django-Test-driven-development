57a650753f703b926bea7c92d50a6b53cb30d89c
buffer0x7cd <manvendra021997@gmail.com>
2017-10-05 14:06:29 +0530

    Functional test fails when twi items were added in to do list

--- functional_tests.py
+++ functional_tests.py
@@ -33,6 +33,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
+        time.sleep(1)
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
@@ -40,18 +41,33 @@ class NewVisitorTest(unittest.TestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New To-Do Item did not appear in the list"
+            "New to-do item did not appear in table. \
+            Contents were: \n {0}".format(table.text)
         )
 
-        # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.fail('Finish the test')
+        # There is still a text box inviting her to add another item.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a To-Do item'
+        )
+        # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = self.browser.find_elements_by_tag_name('tr')
+        self.assertIn(
+            '1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly', [row.text for row in rows])
 
 
 
 
 
-# The page updates again, and now shows both items on her list
 
 # Edith wonders whether the site will remember her list. Then she sees
 # that the site has generated a unique URL for her -- there is some
--- lists/tests.py
+++ lists/tests.py
@@ -9,3 +9,11 @@ class HomePageTest(TestCase):
         ''' Check if correct template is being used for render'''
         responce = self.client.get('/')
         self.assertTemplateUsed(responce, 'lists/home.html')
+        
+    def test_can_save_a_POST_request(self):
+        ''' Test if view echo back the post data'''
+        responce = self.client.post('/', data={
+            'item_text': 'A new list item'
+        })
+        self.assertIn('A new list item', responce.content.decode())
+        self.assertTemplateUsed(responce, 'lists/home.html')
