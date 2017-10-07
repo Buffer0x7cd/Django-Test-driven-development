c2e885f2686774b861466ab5f8b4d162c8912a73
buffer0x7cd <manvendra021997@gmail.com>
2017-10-08 04:13:31 +0530

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -108,9 +108,20 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
         # of Edith's is coming through from cookies etc
-# Edith wonders whether the site will remember her list. Then she sees
-# that the site has generated a unique URL for her -- there is some
-# explanatory text to that effect.
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
+        # She visits that URL - her to-do list is still there.
 
-# She visits that URL - her to-do list is still there.
+    def test_style_and_layout(self):
+        ''' Test the style and layout of the site'''
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] /2, 512, delta=10)
