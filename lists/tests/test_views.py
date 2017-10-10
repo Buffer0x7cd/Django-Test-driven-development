24f18b75f9aa4b0e07039ad664ccb6104649b8be
buffer0x7cd <manvendra021997@gmail.com>
2017-10-10 16:43:40 +0530

    Seperated unit tests into two different files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -3,7 +3,7 @@ from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from .models import Item, List
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -12,36 +12,6 @@ class HomePageTest(TestCase):
         ''' Check if correct template is being used for render'''
         responce = self.client.get('/')
         self.assertTemplateUsed(responce, 'lists/home.html')
-
-class ListAndItemModelTest(TestCase):
-    ''' Test the item model'''
-
-    def test_saving_and_retrieving_items(self):
-        list_ =  List()
-        list_.save()
-        first_item = Item()
-        first_item.text = "The first (ever) list item"
-        first_item.item_list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = "Item the second"
-        second_item.item_list = list_
-        second_item.save()
-
-        saved_lists = List.objects.first()
-        self.assertEqual(saved_lists, list_)
-
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.item_list, list_)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.item_list, list_)
 
 
 class LiveViewTest(TestCase):
