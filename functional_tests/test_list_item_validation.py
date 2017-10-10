a344ff928daf1be812f50f39f2aceffe7e95d69c
buffer0x7cd <manvendra021997@gmail.com>
2017-10-10 16:31:39 +0530

    Added a helper function for generic wait functionality in base. Added test for item validation in test_list_item_validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,11 +1,43 @@
 from selenium.webdriver.common.keys import Keys
 from .base import FunctionalTest
 from unittest import skip
-
+from selenium.common.exceptions import WebDriverException
+import time
 
 class ItemValidationTest(FunctionalTest):
-
-    
     def test_cannot_add_empty_list_items(self):
-        ''' some story '''
-        self.fail('write me')
+        ''' Edith goes to the home page and accidentally tries to submit
+        an empty list item. She hits Enter on the empty input box '''
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(Keys.ENTER)
+
+        ''' The home page refreshes, and there is an error message saying
+        that list items cannot be blank'''
+
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            'yo can not enter an empty list item'
+        ))
+
+        ''' She tries again with some text for the item, which now works'''
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: buy milk')
+
+        '''  she tries to enter a blank line again'''
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        ''' again she recieves an error message'''
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            'yo can not enter an empty list item'
+        ))
+        ''' and she can correct it by filling some text in'''
+        inputbox =  self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('buy mango')
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: buy milk')
+        self.check_for_row_in_list_table('2: buy mango')
+        self.fail('Finish this test')
+
